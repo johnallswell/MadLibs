f146f18a289de1c12df141868d7c9a62e4c210d9
johnallswell4 <johnsannithomas@gmail.com>
2015-03-23 23:55:30 -0400

    Add print out result using replaceWith()

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
 $(document).ready(function(){
   $("#submit-btn").click(function(){
+    var exclamation = $("#exclamation").val();
+    var adverb = $("#adverb").val();
+    var noun = $("#noun").val();
+    var verb = $("#verb").val();
 
-    console.log("Hello, World!!");
+
+    var mad_lib = "'" + exclamation + "!' he said, as he " + adverb + " jumped onto his " + noun + " and " + verb + " into the sunset.";
+    $("#mad_lib").hide();
+    $("#mad_lib").replaceWith('<p class="lead top-margin centered" id="result">' + mad_lib + '</p>').fadeIn("slow");
+    
 
   });
-
-
 });
